db58696b2e85ed71d4cd6efc8b008a28c4ec2a74
Jusa <sogijustin@gmail.com>
2025-12-31 18:27:59 +0200

    Delete ampz-app directory

--- ampz-app/server/src/routes/tickets.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { purchaseTicket, getMyTickets } from '../controllers/ticketController';
-
-const router = express.Router();
-
-router.post('/purchase', purchaseTicket);
-router.get('/user/:userId', getMyTickets);
-
-export default router;
